bc5852fa55979eecf3e4a9c770fc495264f80b1e
Ikraam <ikraamghoor@gmail.com>
2020-04-13 16:39:04 +0200

    Quote command added

--- bin/main.rb
+++ bin/main.rb
@@ -12,8 +12,7 @@ Telegram::Bot::Client.run(token) do |bot|
     chat_id = message.chat.id
     case message.text
     when %r{^/start}
-      sent_message = bot.api.send_message(chat_id: chat_id, text: "Hi there!\nGreat to meet you #{message.from.first_name} 😁 \nI'm going to send you a little gratitude quote everyday.\nIf you would like me to pause/re-start the reminders, you can reply with /stop and /start")
-      # bot.api.send_message(chat_id: chat_id, text: "Quote of the day:\n#{file_data[rand(1...file_data.size)]}")
+      sent_message = bot.api.send_message(chat_id: chat_id, text: "Hi there!\nGreat to meet you #{message.from.first_name} 😁 \nI'm going to send you a little gratitude quote everyday. Get one immediately by typing /quote\nYou can write or view a gratitude entry by typing /write and /view\nIf you would like me to pause or re-start the reminders, you can reply with /stop and /start")
       Entries.new(chat_id)
       puts message.from.first_name
 
@@ -23,7 +22,7 @@ Telegram::Bot::Client.run(token) do |bot|
       Entries.new(chat_id).remove_user(chat_id)
 
     when %r{^/write}
-      bot.api.send_message(chat_id: chat_id, text: "What happy thoughts would you like to journal?\n(I will randomly remind you of this event in the future to bring a smile to your face 😇)\n ")
+      bot.api.send_message(chat_id: chat_id, text: "What are you grateful for?\n(I will randomly remind you of this entry in the future to bring a smile to your face 🥳)\nTo cancel this entry type /cancel")
 
       Write.new(chat_id)
 
@@ -34,8 +33,9 @@ Telegram::Bot::Client.run(token) do |bot|
 
     when %r{^/view}
       bot.api.send_message(chat_id: chat_id, text: " Here are the entries: #{StoreMessage.get_messages(chat_id)}")
-      StoreMessage.get_messages(chat_id)
 
+    when %r{^/quote}
+      bot.api.send_message(chat_id: chat_id, text: (file_data[rand(1...file_data.size)]).to_s)
     else
       if Write.in_write_state?(chat_id)
         Write.new(chat_id).remove_user(chat_id)
