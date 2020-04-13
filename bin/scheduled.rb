b111c23a8da67c028c1298e435c59fae93714975
Ikraam <ikraamghoor@gmail.com>
2020-04-13 15:04:59 +0200

    Fixed file path to quotes in db

--- bin/scheduled.rb
+++ bin/scheduled.rb
@@ -3,15 +3,15 @@
 
 require 'telegram/bot'
 require_relative '../lib/entries.rb'
-file_data = File.read('quotes.txt').split("\n")
+file_data = File.read('./db/quotes.txt').split("\n")
 
 token = '1231002234:AAGAlG8tDpS6M7JOe9WNeEJWvCQVglvLJV0'
 
 token = '1231002234:AAGAlG8tDpS6M7JOe9WNeEJWvCQVglvLJV0'
 
 loop do
-  users = Entries.users
-  # users = [1_147_048_017, 296_643_681]
+  # users = Entries.users
+  users = [1_147_048_017, 296_643_681]
   i = 0
   while i < users.size
     user = users[i].to_i
