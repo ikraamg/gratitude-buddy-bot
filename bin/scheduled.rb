908c8980ad3268a12924040a42dc2f15a12a341d
Ikraam <ikraamghoor@gmail.com>
2020-04-14 15:24:00 +0200

    Refactored scheduled.rb

--- bin/scheduled.rb
+++ bin/scheduled.rb
@@ -1,25 +1,24 @@
 #!/usr/bin/env ruby
 require 'telegram/bot'
-require_relative '../lib/entries.rb'
+require_relative '../lib/state_manager.rb'
 require 'dotenv/load'
-file_data = File.read('./db/quotes.txt').split("\n")
 
 token = ENV['TELEGRAM_API_KEY']
 
-loop do
-  users = Entries.users
-  # users = [1_147_048_017, 296_643_681]
-  i = 0
-  while i < users.size
-    user = users[i].to_i
+file_data = File.read('./db/quotes.txt').split("\n")
 
+loop do
+  users = StateManager.items_in_managed_state('users')
+
+  users.each do |user|
     Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: user, text: "Quote of the day:\n#{file_data[rand(1...file_data.size)]}")
     end
+
     p user
     sleep(1)
-    i += 1
   end
+
   puts Time.now
   sleep(14_400)
 end
