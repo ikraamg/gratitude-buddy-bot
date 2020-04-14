411acdd3cf32c2d0fb5fa592442816f22a76d111
Ikraam <ikraamghoor@gmail.com>
2020-04-14 15:06:56 +0200

    Created StateManager or scheduled and write users

---
+++ lib/state_manager.rb
@@ -0,0 +1,31 @@
+require_relative './database_management'
+# include DatabaseManagement
+
+class StateManager
+  include DatabaseManagement
+  extend DatabaseManagement
+
+  def initialize(message_object, state_managed)
+    @chat_id = message_object.chat.id
+    @state_managed = state_managed
+  end
+
+  def true_state
+    append_to_file(@state_managed, @chat_id)
+  end
+
+  def state?
+    contained_in_file?(@state_managed, @chat_id)
+  end
+
+  def false_state
+    remove_from_file(@state_managed, @chat_id)
+  end
+
+  def self.items_in_managed_state(state_managed)
+    file_to_array(state_managed)
+  end
+end
+
+# #Tests
+# puts StateManager.items_in_managed_state('users')
