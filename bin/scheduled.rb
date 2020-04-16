20643a6d6f1e9b05c4bc877b01fdaee3728e7628
Ikraam <ikraamghoor@gmail.com>
2020-04-16 11:24:37 +0200

    Removed commented code and debug code

--- bin/scheduled.rb
+++ bin/scheduled.rb
@@ -3,12 +3,11 @@ require 'telegram/bot'
 require_relative '../lib/state_manager.rb'
 require 'dotenv/load'
 
-# apikey = ENV['TELEGRAM_API_KEY']
-
 file_data = File.read('./db/quotes.txt').split("\n")
 
 def send_message(message, user)
-  Telegram::Bot::Client.run(ENV['TELEGRAM_API_KEY']) do |bot|
+  apikey = ENV['TELEGRAM_API_KEY']
+  Telegram::Bot::Client.run(apikey) do |bot|
     bot.api.send_message(chat_id: user, text: message)
   end
 end
@@ -18,22 +17,16 @@ loop do
 
   users.each do |user|
     send_message("Quote of the day:\n#{file_data[rand(0...file_data.size)]}", user)
-    p user
     sleep(1)
   end
-  puts 'quote sent'
-  puts Time.now
   sleep(28_800)
 
   users = StateManager.items_in_managed_state('users')
 
   users.each do |user|
     send_message("What were you grateful for today? 😊\nSend /write to log an entry", user)
-    p user
     sleep(1)
   end
-  puts 'write reminder'
-  puts Time.now
   sleep(28_800)
 
   users = StateManager.items_in_managed_state('users')
@@ -43,11 +36,8 @@ loop do
 
     user_entries = File.read("./db/#{user}.txt").split("\n")
     send_message("A past entry from your gratitude journal 🥳:\n #{user_entries[rand(0...user_entries.size)]}", user)
-    p user
     sleep(1)
   end
 
-  puts 'past entry reminder'
-  puts Time.now
   sleep(28_800)
 end
